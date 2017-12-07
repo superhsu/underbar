25727d09d3763d346e7b57923ee27f9a411bfb11
superhsu <31141265+superhsu@users.noreply.github.com>
2017-12-06 19:54:58 -0800

    second commit underbar part 1

--- src/underbar.js
+++ src/underbar.js
@@ -79,12 +79,30 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = []; 
+    for (var i = 0; i < collection.length; i++) {
+      if(test(collection[i]) === true) {
+        result.push(collection[i]);
+      }
+    }
+    return result;
+
+   // _.each(collection, function(item) {
+    //  if (test(item) === true) {
+     //   result.push(item); 
+     // }
+    //})
+   // return result; 
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    return _.filter(collection, function(item) {
+      return test(item) === false; 
+      }); 
   };
 
   // Produce a duplicate-free version of the array.
